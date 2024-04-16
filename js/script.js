c4207c0d29b6ad14a6faf49f4c5ee7a715ae4bac
giordan-zeina <144264315+giordan-zeina@users.noreply.github.com>
2024-04-16 18:41:10 +0000

    done

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Giordan Zeina
+// Created on: Mar 2024
 // This file contains the JS functions for index.html
+
+'use strict'
+/**
+ * This function calculates area of a trapezoid
+ */
+function myButtonClicked() {
+  // input
+  const aBase = parseFloat(document.getElementById('a-base').value)
+  const bBase = parseFloat(document.getElementById('b-base').value)
+  const height = parseFloat(document.getElementById('height').value)
+
+  // process
+  const area = ((aBase + bBase) / 2) * height
+
+  // output
+  document.getElementById('area').innerHTML = 'The area is: ' + area + ' mm²'
+}
